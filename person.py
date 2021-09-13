2b5152f46428f0a4452384572a202082c48e94dc
RemcoDrolenga <rdrolenga@gmail.com>
2021-09-13 11:11:11 +0200

    Bestand en Screenshot toegevoegd aan de repo

---
+++ person.py
@@ -0,0 +1,11 @@
+Name = input('Vul hier je naam in: ')    
+Adres = input('Vul hier je adres in: ')
+Postcode = input('Vul hier je postcode in: ')
+Woonplaats = input('Vul hier je woonplaats in: ')
+
+print(' ----------------------------------------------------')
+print(' |  Naam      : ' + str(Name)                         )
+print(' |  Adres     : ' + str(Adres)                        )
+print(' |  Postcode  : ' + str(Postcode)                     )
+print(' |  Woonplaats: ' + str(Woonplaats)                   )
+print(' ----------------------------------------------------')
